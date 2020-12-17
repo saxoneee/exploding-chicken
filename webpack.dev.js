ea13e0ac1c0e5d36ab3cda8a0f7bd610fb5eb984
Hendrik Sachse <hendrik.sachse@gmail.com>
2020-12-17 23:36:19 +0100

    cleanup webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,4 @@
 var path = require('path'),
-    CopyPlugin = require('copy-webpack-plugin'),
     { CleanWebpackPlugin } = require('clean-webpack-plugin'),
     HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -11,49 +10,40 @@ var cfg = {
 module.exports = {
     entry: path.join(cfg.srcPath, 'ts/main.ts'),
     devtool: 'inline-source-map',
-	output: {
+    output: {
         path: cfg.distPath,
         filename: '[name].[contenthash].js'
-	},
-
-	devServer: {
-		open: false,
-		contentBase: [
-			cfg.srcPath,
-		],
-		port: 8000,
     },
-    
+
+    devServer: {
+        open: false,
+        port: 8000,
+    },
+
     module: {
         rules: [
             {
-              test: /\.tsx?$/,
-              use: 'ts-loader',
-              exclude: /node_modules/,
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
-              },
-              {
+            },
+            {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
-              },
+            },
         ],
     },
 
     resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ],
+        extensions: ['.tsx', '.ts', '.js'],
     },
 
-	plugins: [
+    plugins: [
         new CleanWebpackPlugin(),
-        /*
-        new CopyPlugin({
-            patterns: [
-              { from: path.join(cfg.srcPath, 'assets'), to: path.join(cfg.distPath, 'assets') },
-            ],
-        }),//*/
         new HtmlWebpackPlugin()
-      ]
+    ]
 };
